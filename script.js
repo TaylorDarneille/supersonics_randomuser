d6fab196a17e58f2b9bcc50876a57a0d09ae7447
Taylor Darneille <tmdarneille@gmail.com>
2021-01-06 16:05:20 -0800

    getting 10 random users displaying in list

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-console.log("Hello from Fetch Practice!")
+// console.log("Hello from Fetch Practice!")
 
-const redditEndpoint = 'https://www.reddit.com/search.json?q=kittens'
+// const redditEndpoint = 'https://www.reddit.com/search.json?q=kittens'
 
-fetch(redditEndpoint)
-.then((fetchObj)=>{
-    console.log("Here is the fetch object:", fetchObj)
-    return fetchObj.json()
+// fetch(redditEndpoint)
+// .then((fetchObj)=>{
+//     console.log("Here is the fetch object:", fetchObj)
+//     return fetchObj.json()
+// })
+// .then((jsonData)=>{
+//     console.log("Here is the json Data:", jsonData)
+// })
+// .catch((error)=>{
+//     console.log("oh no, you did NOT make fetch happen")
+//     console.log(error)
+// })
+
+// console.log("Just fired the AJAX request")
+
+// // const taylorsFunction = (bands) => {
+// //     bands.forEach((band)=>{
+// //         console.log(band)
+// //     })
+// // }
+
+// // taylorsFunction(['Hot Mulligan', 'Local Natives'])
+
+const addPerson = (person) => {
+    // let peopleList = document.getElementById('peopleList')
+    let newPerson = document.createElement('li')
+    newPerson.textContent = `${person.name.first} ${person.name.last}`
+    peopleList.appendChild(newPerson)
+}
+
+const randomUserEndpoint = 'https://randomuser.me/api/?results=10'
+
+fetch(randomUserEndpoint)
+.then((fetchedUsers)=>{
+    return fetchedUsers.json()
 })
-.then((jsonData)=>{
-    console.log("Here is the json Data:", jsonData)
+.then((jsonUsers)=>{
+    jsonUsers.results.forEach(addPerson)
+    // console.log(jsonUsers.results)
 })
-.catch((error)=>{
-    console.log("oh no, you did NOT make fetch happen")
-    console.log(error)
+.catch((err)=>{
+    console.log('Failed to fetch users!', err)
 })
-
-console.log("Just fired the AJAX request")
-
-// const taylorsFunction = (bands) => {
-//     bands.forEach((band)=>{
-//         console.log(band)
-//     })
-// }
-
-// taylorsFunction(['Hot Mulligan', 'Local Natives'])
